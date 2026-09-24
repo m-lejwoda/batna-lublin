209ea6bf5a47c73740162053dfa992cd5ed21533
Michał <michal.lejwoda@gmail.com>
2026-09-24 21:45:55 +0200

    Add models

--- internal/models/investment.go
+++ internal/models/investment.go
@@ -3,9 +3,48 @@ package models
 import "time"
 
 type Investment struct {
-	ID             int64
+	ID                      string
+	Name                    string
+	FinishDate              time.Time
+	OfficeLocation          string
+	Phone                   string
+	UndergroundParkingPlace bool
+	SurfaceParkingPlace     bool
+	Garage                  bool
+}
+
+type Flat struct {
+	ID             string
 	Name           string
-	FinishDate     time.Time
-	OfficeLocation string
-	Phone          string
+	FloorArea      int64
+	Layout         string
+	Floor          int64
+	FlatNumber     string
+	BuildingNumber string
+	RoomsNumber    int64
+	Balcony        bool
+	InvestmentID   string
+}
+
+type ParkingPlace struct {
+	ID           string
+	Type         string
+	FloorArea    int64
+	InvestmentID string
+}
+
+type FlatStatus struct {
+	Price     int64
+	Currency  string
+	Status    string
+	FlatID    string
+	CreatedAt time.Time
+}
+
+type ParkingPlaceStatus struct {
+	Price          int64
+	Currenct       string
+	Status         string
+	CreatedAt      time.Time
+	ParkingPlaceID string
 }
